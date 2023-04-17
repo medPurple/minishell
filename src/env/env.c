6a56ea408b6decdb5b5d54604b54a1c3802766fe
Marie-Laure Vautrot <vautrot.marielaure@gmail.com>
2023-04-17 17:32:48 +0200

    notes parsing + env

--- src/env/env.c
+++ src/env/env.c
@@ -1,5 +1,14 @@
-/*get env le path
-creer liste chainee recuperant l env
+/*
+gestion environnement :
+renvoyer les variables minimum indiquee dans le doc shell (cf doc)
+gestion de la creation d une nouvelle variable et l'ajouter a la suite
+de l'environnement
+
+=> get env le path
+et creer liste chainee recuperant l env ?
+avec un titre et un contenu. exemple imprimer $FOO ( if $FOO =>search name liste chainee)
+
+
 
 
 */
---
+++ src/parsing/parsing.c
@@ -0,0 +1,10 @@
+
+/*
+si mauvaise commande == foo => afficher message d erreur et renvoyer un prompt
+si bonne commande == gestion des flags dans les commandes : ls -l -a- F ou ls -laF doivent etre interpretes de la meme facon par exemple // cad donner le mm resultat // puis executer et renvoyer un prompt
+					== gestion des built in ? si built in -> aller chercher ces fonctiosn et faire la meme.
+		gestion du tild (~) ?
+
+
+
+*/
