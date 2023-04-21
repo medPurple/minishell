aa63376f37f7f0541a8a0c83bd7b0ddda511a81b
Marie-Laure <vautrot.marielaure@gmail.com>
2023-04-21 15:20:47 +0200

    crea fnct lst + recup env

--- src/env/env.c
+++ src/env/env.c
@@ -8,10 +8,30 @@ de l'environnement
 et creer liste chainee recuperant l env ?
 avec un titre et un contenu. exemple imprimer $FOO ( if $FOO =>search name liste chainee)
 
-
-
-
 */
 #include "../../include/minishell.h"
 
-
+t_env *add_env(t_env *env, char **envp)
+{
+    int i;
+    t_env *tmp;
+    // creer fonction d initialisation ?
+    env->name = NULL;
+    env->data = NULL;
+     
+    i = 0;
+    if (envp && envp[i])
+    {
+        while (envp[i])
+        {
+            tmp = ft_new_element(ft_strdup(envp[i]));
+            if (!tmp || !tmp->data)
+            {
+                //clear(&env)
+            }
+            ft_add_back_lst(&env, tmp);
+            i++;
+        } 
+    }
+    return (env);
+}
---
+++ src/lst/lst_create.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lst_create.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ml <ml@student.42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/21 15:14:16 by ml                #+#    #+#             */
+/*   Updated: 2023/04/21 15:14:41 by ml               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+t_env	*ft_new_element(char *data)
+{
+	t_env	*element;
+
+	element = malloc(sizeof(t_env));
+	if (!element)
+		return (NULL);
+	element->data = data;
+	element->next = NULL;
+	return (element);
+}
+
+t_env	*ft_last_lst(t_env *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+	{
+		lst = lst->next;
+	}
+	return (lst);
+}
+
+void	ft_add_back_lst(t_env **lst, t_env *new)
+{
+	t_env	*str;
+
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		str = ft_last_lst(*lst);
+		str->next = new;
+	}
+}
