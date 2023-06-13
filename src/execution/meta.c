fc929058f0501455f3b6c495a3c5a9a18494eabb
purple <purple.pro@gmail.com>
2023-06-13 16:47:42 +0200

    or ok

--- src/execution/meta.c
+++ src/execution/meta.c
@@ -4,7 +4,10 @@ void mini_or(t_binary *tree, t_minishell *mini)
 {
     if (tree->prev->prev->left->cmd->exec == true)
     {
-        tree->prev->right->left->cmd->exec = true;
+        if (tree->prev->right->right)
+            tree->prev->right->left->cmd->exec = true;
+        else
+            tree->prev->right->cmd->exec = true;
         return;
     }
     else
